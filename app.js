245b7551de846d3a2e1ecde4071737b1be846e5c
Michael Shebeko Admin <mshebeko@gmail.com>
2018-01-15 00:17:56 +0200

    Repo init

---
+++ app.js
@@ -0,0 +1,63 @@
+const express =require ('express');
+const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost/yelp_camp");
+app.set('view engine','ejs'); 
+app.use(bodyParser.urlencoded({extended: true}));
+
+//SCHEMA SETUP
+var campgroundSchema = new mongoose.Schema({
+	name: String,
+	image: String,
+	description: String
+});
+var Campground = mongoose.model("Campground",campgroundSchema)
+
+app.get('/',function(req,res){
+	res.render('landing');
+});
+
+app.get('/campgrounds',function(req,res){
+	Campground.find({},function(err,allCampgrounds){
+		if(err){
+			console.log(err);
+		} else {
+			res.render('index',{campgrounds:allCampgrounds});
+		}
+	});	
+});
+
+app.post('/campgrounds',function(req,res){
+	var name = req.body.name
+	var image = req.body.image
+	var description = req.body.description
+	var newCampground = {name: name, image: image, description: description}
+	Campground.create(newCampground,function(err,newlyCreated){
+		if (err){
+			console.log(err)
+		} else{
+			res.redirect('/campgrounds')
+		}
+	})
+});
+
+app.get('/campgrounds/new',function(req,res){
+	res.render("new.ejs")
+});
+
+app.get('/campgrounds/:id',function(req,res){
+	var campId = req.params.id
+	Campground.findById(campId, function(err,campground){
+		if (err){
+			console.log(err)
+		} else {
+			res.render('show',{campground: campground})
+		}
+	})
+});
+
+app.listen(3000,function(){
+	console.log('Yelp Camp Started')
+})
